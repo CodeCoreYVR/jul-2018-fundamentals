1ae2decea67197a6a5bf25a6e9a8961a9ee9c087
Jacob Tran <jacobtran@gmail.com>
2018-07-21 11:43:44 -0700

    add day 1 updates

---
+++ day1/1_text.js
@@ -0,0 +1,38 @@
+// text!!!
+
+// strings!!!
+"An ancient ancestor told us to not eat the red mushrooms."
+
+
+"Jacob"
+
+"J"-"a"-"c"-"o"-"b"
+
+// three ways to create strings
+"Jacob";
+'Tran';
+// above the tab key on the keyboard
+// back tick
+`hello!`;
+
+
+"This is Jacob's dog!";
+'"Hello," said Jacob.';
+`"This is Jacob's dog," said Summer."`;
+
+// escaping
+'This is Jacob\'s dog';
+
+
+"Jacob Tran";
+
+
+
+
+
+
+
+
+
+
+//
---
+++ day1/2_variables.js
@@ -0,0 +1,34 @@
+// variables!!!
+// changes
+
+// it is a spot in memory where can hold information
+var name = "Jacob";
+console.log(name);
+name = "Prince";
+console.log(name);
+name = 39;
+console.log(name);
+
+var name = "Jacob";
+var name = "Rosa";
+
+// let
+let light = true;
+let light = false;
+
+// constant - doesn't change, fixed
+const PI = 3.14159;
+PI = 42;
+
+// const - amazon
+const primeMembershipFee = 70;
+const provTax = 0.07;
+const gst = 0.05;
+
+
+
+
+
+
+
+//
