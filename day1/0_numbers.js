5d5a5c98eb6b41001eff26474d8cca266ebbb356
Jacob Tran <jacobtran@gmail.com>
2018-07-21 11:43:44 -0700

    add day 1 updates

---
+++ day1/0_numbers.js
@@ -0,0 +1,89 @@
+// basic
+// fortran
+// natural programming languages
+// natural language processing (NLP)
+
+
+// javascript
+// 10 day, brendan eich
+
+
+// ECMAScript 6.0
+// ECMAScript 8.0
+
+// JavaScript - you get sued!??!
+// Oracle vs. Google
+
+// JavaScript - is the only native programming language for the web
+// Java is #1
+
+// numbers
+// 0's and 1's
+
+// binary
+// 0
+// 1
+// 01
+// 10
+// 11
+// 001
+// ...
+
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+1 + 1; // instructions
+2 + 4;
+6 - 3;
+10 / 2;
+4 * 5;
+
+1 + 1; // instructions
+2 + 4;
+6 - 3;
+10 / 2;
+4 * 5;
+
+console.log(1 + 1);
+console.log(2 + 4);
+console.log(6 - 3);
+console.log(10 / 2);
+console.log(4 * 5);
+
+// console.log(2^4);
+console.log(2 ** 4);
+console.log( Math.pow(2, 4) );
+
+Math.random();
+Math.round( Math.random() * 10 );
+
+// single line commenting
+// communicating to yourself or other programmers
+
+/*
+
+   A long comment
+	 that may span
+	 multiple lines
+
+*/
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+//
---
+++ day1/3_gating.js
@@ -0,0 +1,42 @@
+// gating
+
+// booleans - 2 states
+true; // 1
+false; // 0
+
+if (true) {
+  console.log("welcome the farm!");
+}
+
+if (false) {
+  console.log("welcome the farm!");
+}
+
+1 > 2;
+3 > 2;
+
+
+if (1 > 2) {
+  console.log("welcome the farm!");
+}
+
+
+if (true) {
+  console.log("welcome to the farm");
+} else {
+  console.log("goodbye");
+}
+
+let age = 39;
+
+if (age > 38) {
+  console.log("middle aged");
+} else (age > 65) {
+  console.log("senior");
+}
+
+
+
+
+
+//
