bc2238981d32b9a00538baab83d21b41f9129849
Jacob Tran <jacobtran@gmail.com>
2018-07-28 11:06:28 -0700

    add arrays

---
+++ day3/0_day3.js
@@ -0,0 +1,3 @@
+// 1. arrays
+// 2. functions
+// 3. objects
---
+++ day3/2_multi-dimensional.js
@@ -0,0 +1,27 @@
+// multidimensional arrays
+let row1 = [null, null, null];
+let row2 = [null, null, null];
+let row3 = [null, null, null];
+
+let ticTacToe = [row1, row2, row3];
+
+let ticTacToe = [
+  [null, null, null], // 0
+  // 0    1     2
+  [null, null, null], // 1
+  [null, null, null] // 2
+];
+
+// ticTacToe[row?][col?]
+ticTacToe[1][1] = "X";
+ticTacToe[0][0] = "O";
+ticTacToe[0][2] = "X";
+ticTacToe[0][1] = "O";
+ticTacToe[1][2] = "X";
+ticTacToe[2][0] = "O";
+ticTacToe[2][2] = "X";
+
+// X wins!!!
+
+
+//
