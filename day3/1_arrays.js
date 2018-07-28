bcd30b4f1c4e0ad9f0165bbcb943e1c01c12be77
Jacob Tran <jacobtran@gmail.com>
2018-07-28 11:06:28 -0700

    add arrays

---
+++ day3/1_arrays.js
@@ -0,0 +1,63 @@
+// what is an array?
+
+// an indexed list of information.
+
+// to do list
+// grocery shopping
+// packing list
+// class list
+
+
+// milk, eggs, bread, apples, oranges, ice cream
+
+//[] // sub, array initalizer
+
+let shoppingList = ["milk", "eggs", "bread"];
+
+let name = "Jacob";
+//              0        1
+let names = ["Jacob", "Tran"];
+
+// access the array
+names[0]; // "Jacob"
+names[1]; // "Tran"
+
+ //                   0       1        2
+let shoppingList = ["milk", "eggs", "bread"];
+shoppingList[2];
+shoppingList[3];
+
+shoppingList[2]; // getter
+shoppingList[2] = "butter"; // setter
+
+console.log( shoppingList );
+console.table( shoppingList ); // only for javascript console
+
+// one dimensional array
+let shoppingList = ["milk", "eggs", "bread"];
+shoppingList[0]; // only one piece of information is needed to get to the data
+
+// two dimensional array
+// you need 2 pieces of information to get to the data
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+//
---
+++ day3/review.js
@@ -0,0 +1,34 @@
+// good morning
+
+// 1. who created javascript?
+// brendan eich
+
+// 2. what are the 3 ways to create variables in javascript?
+// let, const, var
+
+let a = 10;
+let a = 20; // will throw an error
+
+var a = 10;
+var a = 20; // is allowed
+
+const PI = 3.14159;
+PI = 42; // will throw an error
+
+// 3. how can you stop this from being an infinite loop?
+
+let counter = 0;
+
+while (counter <= 10) {
+  console.log(counter);
+  counter++;
+}
+
+// counter = counter + 1;
+
+
+
+
+
+
+//
