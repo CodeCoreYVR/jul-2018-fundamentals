e59778fc5b3196266e83019f86e9ce7ef4bcf3be
Jason <jzntam@gmail.com>
2018-07-29 19:23:11 -0700

    Add day 3 lab solutions

---
+++ day3/lab_solutions/largest_number_smallest_number.js
@@ -0,0 +1,28 @@
+// Assignment: [lab] Largest number and smallest number
+// Write a function that takes in an array and returns another whose first
+// element is the smallest number and whose second element is the largest number.
+
+// For example if you call the function with array [3,64,3,56,34,12,5,0]
+// you should get back an array [0, 64]
+
+
+function smallestAndLargestNumber(numbers) {
+  let smallest = numbers[0];
+  let largest = numbers[0];
+
+  for (let number of numbers) {
+    if (number > largest) {
+      largest = number;
+    }
+
+    if (number < smallest) {
+      smallest = number;
+    }
+  }
+
+  return [smallest, largest];
+};
+
+const numbersArray = [3, 64, 3, 56, 1004, 34, 12, 5, 6];
+
+smallestAndLargestNumber(numbersArray);
