27570dea369aea66652eb640581b0527db09e1e4
Jacob Tran <jacobtran@gmail.com>
2018-08-18 11:16:30 -0700

    add day 8

---
+++ day8/1_exercises.js
@@ -0,0 +1,18 @@
+// css for selecting all the shapes
+// style.css
+// class
+// .shape {
+//   display: none;
+// }
+// // id
+// #purple-container {
+//   display: flex;
+// }
+// // tag name
+// h1 {
+//   color: blue;
+// }
+
+// into the $() goes a css selector as a string
+$('.shape');
+$('????');
---
+++ day8/review.js
@@ -0,0 +1,13 @@
+// good morning!
+
+// unification
+// holy trinity: html+css+js
+document.write("Hello Fundamentals!");
+
+// vanilla js
+// document.getElementById().innerHTML = "Jacob was here!";
+// document.getElementsByTagName()
+// document.getElementsByClassName()
+
+// jquery
+$('h1').html("Jacob was here");
